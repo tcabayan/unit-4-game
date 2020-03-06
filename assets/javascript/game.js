170036766f4cf4605ba1660c5c8b2360a9302806
Tamar Cabayan <tcabayan@gmail.com>
2020-03-05 22:41:57 -0800

    Made some changes to css styling and updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var losses;
 var numbers;
 var randomNumber;
 var found;
+var status;
 
 //determining win and loss counter
 $('#wins-tracker').text(wins);
@@ -13,20 +14,15 @@ $('#loss-tracker').text(losses);
 function newCrystals() {
     numbers = [];
     while(numbers.length < 4){
-        //Math.ceil will round the number in to an integer
-        //here we will going to determine the random number 
-        //this number is the wining number if the user gusse correctly
-        randomNumber = Math.ceil(Math.random()*15)
-        console.log(randomNumber)
+        randomNumber = Math.floor(Math.random()*13)
         found = false;
         for (var i=0; i< numbers.length; i++){
-            if (numbers[i] == randomNumber){
+            if (numbers[i] === randomNumber){
                 found = true; break
             }
           }
           if(!found)numbers[numbers.length]=randomNumber;
         }
-        console.log(newCrystals);		
     for (i = 0; i < numbers.length; i++) {
         var imageCrystal = $('<img>');
         imageCrystal.attr('data-num', numbers[i]);
@@ -38,7 +34,6 @@ function newCrystals() {
 }
 
 function newGame() {
-
     counter = 0;
     $('#score').text(counter);
 
@@ -51,25 +46,22 @@ function newGame() {
     $('#value').text(numberToGuess);
 
 
-    $('.crystalImage').on('click', function(){
+    $('.crystalImage').on('click', function() {
         counter = counter + parseInt($(this).data('num'));
-       
         $('#score').text(counter);
-//determine user's win 
+        //determine user's win 
         if (counter == numberToGuess){
-          $('#status').text('You won!!!!');
+          $('#status').text('You won!');
           wins ++;
           $('#wins-tracker').text(wins);
-          console.log(wins)
           $('#crystals').empty();
           newCrystals();
           newGame();
-//determine user losses	        
+        //determine user losses	        
         } else if ( counter > numberToGuess){
             $('#status').text('You lost!');
             losses ++;
             $('#loss-tracker').text(losses);
-            console.log(losses)
             $('#crystals').empty();
             newCrystals();
             newGame();
